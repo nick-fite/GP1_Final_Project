b73063db4cff1c9e1aca691d176b2d61a7a9298f
nick-fite <70393136+nick-fite@users.noreply.github.com>
2023-11-15 00:02:20 -0600

    fixed the stupid animation

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -289,8 +289,8 @@ namespace StarterAssets
                 // update animator if using character
                 if (_hasAnimator)
                 {
-                    //_animator.SetBool(_animIDJump, false);
-                    //_animator.SetBool(_animIDFreeFall, false);
+                    _animator.SetBool(_animIDJump, false);
+                    _animator.SetBool(_animIDFreeFall, false);
                 }
 
                 // stop our velocity dropping infinitely when grounded
